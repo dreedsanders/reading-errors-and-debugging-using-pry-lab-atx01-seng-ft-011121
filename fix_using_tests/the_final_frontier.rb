2eadf937626c317c848eb356f1575c1afb04581c
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-03 21:39:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,12 +1,14 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry 
 end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  "Captain's Log, #{star_date}."
 end
 
 def engage
